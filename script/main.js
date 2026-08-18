ed692ed5922433c6227aa221b526f6997aba82a9
yeonjoo4471 <lyj4471@naver.com>
2026-08-18 17:45:33 +0900

    또또또수정

--- script/main.js
+++ script/main.js
@@ -11,4 +11,20 @@ $(function(){
     $('.gnb_item').removeClass('is-active');
     $('.mega_menu').removeClass('is-open');
   });
+
+  const $topBtn = $('.top_btn');
+
+  $(window).on('scroll', function(){
+    if ($(window).scrollTop() > 300) {
+      $topBtn.addClass('is-show');
+    } else {
+      $topBtn.removeClass('is-show');
+    }
+  });
+
+  $topBtn.on('click', function(){
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+  });
 });
